c5bffc01b5fe8692f8bc8a7c3dfaccfc4a8dcc5d
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-15 08:39:28 +0000

    created Users folder, users-model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,19 @@
-const authRouter = require('express').Router();
+const authRouter = require("express").Router();
+const Users = require("../Users/users-model");
+const bcrypt = require("bcryptjs");
 
-authRouter.post('/register', (req, res) => {
+authRouter.post("/register", (req, res) => {
   // implement registration
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+  password = hash;
+  Users.add()
+  .then(user => {
+    res.stat
+  })
 });
 
-authRouter.post('/login', (req, res) => {
+authRouter.post("/login", (req, res) => {
   // implement login
 });
 
